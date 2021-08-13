73b548eedeba9dcd9772dc36337cc43ece9605a3
Kundan Kumar <kundankr94700@gmail.com>
2021-08-13 08:37:36 +0530

    Revision of redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import { increment,decrement,signin } from './Action';
 
 function App() {
+  //
+  const counter = useSelector(state => state.counter)
+  const isLogged = useSelector(state => state.isLogged)
+const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <br></br>
+        <br></br>
+        <div className='form'>
+        {isLogged?<h3 style={{textAlign:'center'}}>Logged In</h3>:" "}
+
+           <h1>counter {counter}</h1>
+        <button className='btn' onClick={()=>dispatch(increment())}>Increase</button>
+        <button className='btn' onClick={()=>dispatch(decrement())}>Decrease</button>
+        <button className='btn' onClick={()=>dispatch(signin())}>{isLogged?'Logout':"Login "}</button>
+       </div>
+      
+
+        
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {createStore,combineReducers} from 'redux';//combine reducers in order to add multiple reducers in a single store
+import allReducers from "./Reducer/index"
+import {Provider} from "react-redux";
+
+const store=createStore(allReducers,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
+
 
 ReactDOM.render(
+  <Provider store={store}>
   <React.StrictMode>
+    <h1 style={{textAlign:'center',margin:'2rem'} }> Redux-Revision</h1>
     <App />
   </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
